a59c40c8aa255fa94ad53095055294064dbc1bac
faisolavolut <faisol@avolut.com>
2025-02-28 10:45:20 +0700

    refactor: remove unused Document Checking pages from dashboard

--- src/app/d/dashboard/page.tsx
+++ src/app/d/dashboard/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { formatMoney } from "@/lib/components/form/field/TypeInput";
 import BarChart from "@/lib/components/ui/bar";
+import { ButtonBetter } from "@/lib/components/ui/button";
 import { CardBetter } from "@/lib/components/ui/card";
 import ProgressChart from "@/lib/components/ui/progress-chart";
-import { getAccess, userRoleMe } from "@/lib/utils/getAccess";
+import { Refresh } from "@/lib/svg/Refresh";
+import { actionToast } from "@/lib/utils/action";
+import { apix } from "@/lib/utils/apix";
 import { getNumber } from "@/lib/utils/getNumber";
 import { useLocal } from "@/lib/utils/use-local";
 import { useEffect } from "react";
@@ -52,23 +55,55 @@ function Page() {
       },
     ],
     time_hire: 1531,
+    filter: {},
+    dashboard: {} as any,
+    refresh: false,
+    reload: async () => {
+      await actionToast({
+        task: async () => {
+          local.dashboard = {};
+          local.refresh = true;
+          local.render();
+          const result = await apix({
+            port: "recruitment",
+            value: "data.data",
+            path: `/api/dashboard`,
+            validate: "object",
+          });
+          local.dashboard = result;
+          local.refresh = false;
+          local.render();
+        },
+        failed: () => {
+          local.refresh = false;
+          local.render();
+        },
+        msg_error: "Failed to retrieve dashboard data",
+        msg_load: "Loading dashboard data",
+        msg_succes: "Dashboard data loaded successfully",
+      });
+    },
   });
 
   useEffect(() => {
-    const run = async () => {
-      const roles = await userRoleMe();
-      local.can_approve = getAccess("approval-verification-profile", roles);
-      local.render();
-    };
-    run();
+    local.reload();
   }, []);
 
   return (
     <div className="flex p-4 flex-col flex-grow gap-y-2">
-      <div className="flex flex-col py-4 pb-0 pt-0">
+      <div className="flex flex-row py-4 pb-0 pt-0">
         <h2 className="text-xl font-semibold text-gray-900 ">
           <span className="">Dashboard</span>
         </h2>
+        <div className="flex-grow flex flex-row items-center justify-end">
+          <ButtonBetter
+            onClick={async () => {
+              local.reload();
+            }}
+          >
+            <Refresh className={cx(local.refresh ? "animate-spin" : "")} />
+          </ButtonBetter>
+        </div>
       </div>
       <div className="flex flex-col flex-grow gap-y-4">
         <div className="grid grid-cols-3 gap-4">
@@ -79,11 +114,18 @@ function Page() {
               </p>
               <div className="flex-grow grid grid-cols-3">
                 <div className="flex flex-row items-center text-2xl font-bold">
-                  {formatMoney(getNumber(local?.recruitment_target?.count))}
+                  {formatMoney(
+                    getNumber(
+                      local?.dashboard?.total_recruitment_target
+                        ?.total_recruitment_target
+                    )
+                  )}
                 </div>
                 <div className="col-span-2 flex flex-row items-center justify-end">
                   <ProgressChart
-                    percentage={getNumber(local?.recruitment_target?.percent)}
+                    percentage={getNumber(
+                      local?.dashboard?.total_recruitment_target?.percent
+                    )}
                     options={{
                       cutout: "70%",
                       plugins: {
@@ -105,13 +147,16 @@ function Page() {
               <div className="flex-grow grid grid-cols-3">
                 <div className="flex flex-row items-center text-2xl font-bold">
                   {formatMoney(
-                    getNumber(local?.recruitment_realization?.count)
+                    getNumber(
+                      local?.dashboard?.total_recruitment_realization
+                        ?.total_recruitment_realization
+                    )
                   )}
                 </div>
                 <div className="col-span-2 flex flex-row items-center justify-end">
                   <ProgressChart
                     percentage={getNumber(
-                      local?.recruitment_realization?.percent
+                      local?.dashboard?.total_recruitment_realization?.percent
                     )}
                     color="#313678"
                     options={{
@@ -132,7 +177,9 @@ function Page() {
                 Bilingual / Non Bilingual
               </p>
               <div className="flex flex-grow text-2xl items-center font-bold">
-                {formatMoney(getNumber(local?.billing))}
+                {formatMoney(
+                  getNumber(local?.dashboard?.total_bilingual?.total_bilingual)
+                )}
               </div>
             </div>
           </CardBetter>
@@ -149,16 +196,15 @@ function Page() {
                   <div className="absolute top-0 left-0 w-full h-full">
                     <BarChart
                       data={{
-                        labels: [
-                          ">30 Hari",
-                          "21-30 Hari",
-                          "11-20 Hari",
-                          "1-10 Hari",
-                        ],
+                        labels:
+                          local?.dashboard?.chart_duration_recruitment
+                            ?.labels || [],
                         datasets: [
                           {
                             label: "Duration",
-                            data: local.duration,
+                            data:
+                              local?.dashboard?.chart_duration_recruitment
+                                ?.datasets || [],
                             backgroundColor: "#313678",
                             borderColor: "#313678",
                             borderRadius: Number.MAX_VALUE,
@@ -204,11 +250,12 @@ function Page() {
                   <div className="absolute top-0 left-0 w-full h-full">
                     <BarChart
                       data={{
-                        labels: local.job_level.map((e) => e?.name),
+                        labels: local?.dashboard?.chart_job_level?.labels || [],
                         datasets: [
                           {
                             label: "Job Level",
-                            data: local.job_level.map((e) => e?.value),
+                            data:
+                              local?.dashboard?.chart_job_level?.datasets || [],
                             backgroundColor: "#313678",
                             borderColor: "#313678",
                             borderRadius: Number.MAX_VALUE,
@@ -233,7 +280,9 @@ function Page() {
                         scales: {
                           x: {
                             min: 0,
-                            max: local.job_level_max,
+                            max: Math.max(
+                              local?.dashboard?.chart_job_level?.datasets || []
+                            ),
                           },
                           y: {
                             display: false, // Menghilangkan sumbu Y
@@ -263,11 +312,14 @@ function Page() {
                   <div className="absolute top-0 left-0 w-full h-full">
                     <BarChart
                       data={{
-                        labels: local.department.map((e) => e?.name),
+                        labels:
+                          local?.dashboard?.chart_department?.labels || [],
                         datasets: [
                           {
                             label: "Department",
-                            data: local.department.map((e) => e?.value),
+                            data:
+                              local?.dashboard?.chart_department?.datasets ||
+                              [],
                             backgroundColor: "#313678",
                             borderColor: "#313678",
                             borderRadius: Number.MAX_VALUE,
@@ -292,7 +344,9 @@ function Page() {
                         scales: {
                           y: {
                             min: 0,
-                            max: 100,
+                            max: Math.max(
+                              local?.dashboard?.chart_department?.datasets
+                            ),
                           },
                         },
                       }}
@@ -311,7 +365,12 @@ function Page() {
 
                 <div className="flex flex-grow relative">
                   <div className="absolute top-0 left-0 w-full h-full flex flex-grow text-2xl justify-center items-center font-bold">
-                    {formatMoney(getNumber(local?.time_hire))} Days
+                    {formatMoney(
+                      getNumber(
+                        local?.dashboard?.avg_time_to_hire?.avg_time_to_hire
+                      )
+                    )}{" "}
+                    Days
                   </div>
                 </div>
               </div>
