e03ca7a94529d0adc773d3bbb42239e0bc98d181
faisolavolut <faisol@avolut.com>
2025-03-05 23:57:37 +0700

    feat: update user assessment field to use multi-async type with improved data fetching

--- src/app/d/fgd/schedule-fgd/[id]/edit/page.tsx
+++ src/app/d/fgd/schedule-fgd/[id]/edit/page.tsx
@@ -392,25 +392,33 @@ function Page() {
                     type={"text"}
                   />
                 </div>
+
                 <div>
                   <Field
                     fm={fm}
                     name={"fgd_schedule_assessors"}
                     label={"User Assessment"}
-                    type={"multi-dropdown"}
-                    onLoad={async () => {
-                      const res: any = await apix({
+                    type={"multi-async"}
+                    onLoad={async (param: any) => {
+                      const params = await events("onload-param", param);
+                      const result: any = await apix({
                         port: "portal",
                         value: "data.data.employees",
-                        path: "/api/employees",
-                        validate: "dropdown",
-                        keys: {
-                          label: "name",
-                        },
+                        path: `/api/employees${params}`,
+                        validate: "array",
                       });
+                      let res = result?.length
+                        ? result.map((e: any) => {
+                            return {
+                              employee_id: e?.id,
+                              employee_name: e?.name,
+                            };
+                          })
+                        : [];
                       return res;
                     }}
-                    required={true}
+                    onValue={"employee_id"}
+                    onLabel={"employee_name"}
                   />
                 </div>
                 <div>
