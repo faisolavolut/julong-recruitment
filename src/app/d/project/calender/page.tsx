2b8f56a61b55b145b917b1d0e182f4042f121355
faisolavolut <faisol@avolut.com>
2025-03-18 13:49:51 +0700

    fix: remove unnecessary console log from calendar page component

--- src/app/d/project/calender/page.tsx
+++ src/app/d/project/calender/page.tsx
@@ -201,7 +201,6 @@ function Page() {
                       };
                     })
                   : [];
-                console.log(result);
                 return result;
               }}
             />
