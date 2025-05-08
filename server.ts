1c8a0943b29c7b9327ecf9712f22a94af3e4dcc3
faisolavolut <faisol@avolut.com>
2025-05-08 14:27:19 +0700

    fix: update development script and correct HTTPS server key/cert paths

--- server.ts
+++ server.ts
@@ -9,15 +9,15 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-  key: fs.readFileSync("./privkey.key"),
-  cert: fs.readFileSync("./cert.crt"),
+  key: fs.readFileSync("./server.key"),
+  cert: fs.readFileSync("./server.cert"),
 };
 
 app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
+  createServer(httpsOptions, (req: { url: any }, res: any) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+  }).listen(3000, (err: any) => {
     if (err) throw err;
     console.log("> Ready on https://localhost:3000");
   });
