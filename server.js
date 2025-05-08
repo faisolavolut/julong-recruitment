6ea6e37093f35ba1cb0d096ae0908efa29caf022
faisolavolut <faisol@avolut.com>
2025-05-08 14:48:52 +0700

    fix: rename server.ts to server.js and update script paths for consistency

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const fs = require("fs");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-  key: fs.readFileSync("./server.key"),
-  cert: fs.readFileSync("./server.cert"),
+  key: fs.readFileSync("./privkey.key"),
+  cert: fs.readFileSync("./cert.crt"),
 };
 
 app.prepare().then(() => {
-  createServer(httpsOptions, (req: any, res: any) => {
+  createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err: any) => {
+  }).listen(3000, (err) => {
     if (err) throw err;
     console.log("> Ready on https://localhost:3000");
   });
